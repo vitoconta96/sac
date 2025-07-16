08fa49e996e4a8b6eae5ec0050da9cab430e17cf
vitoconta96 <vitoconta96@gmail.com>
2025-07-16 11:09:57 +0200

    Update GaugeChart.js

--- GaugeChart.js
+++ GaugeChart.js
@@ -1,30 +1,53 @@
-class GaugeChart extends HTMLElement {
-  constructor() {
-    super();
-    this._value = 50;
-    this.attachShadow({ mode: "open" });
-  }
+(function()  {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Color Properties</legend>
+				<table>
+					<tr>
+						<td>Color</td>
+						<td><input id="bps_color" type="text" size="10" maxlength="10"></td>
+					</tr>
+				</table>
+				<input type="submit" style="display:none;">
+			</fieldset>
+		</form>
+		<style>
+		:host {
+			display: block;
+			padding: 1em 1em 1em 1em;
+		}
+		</style>
+	`;
 
-  set value(val) {
-    this._value = val;
-    this.render();
-  }
+	class BoxBps extends HTMLElement {
+		constructor() {
+			super();
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		}
 
-  connectedCallback() {
-    this.render();
-  }
+		_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							color: this.color
+						}
+					}
+			}));
+		}
 
-  render() {
-    this.shadowRoot.innerHTML = `
-      <canvas id="gauge" width="200" height="100"></canvas>
-    `;
-    const ctx = this.shadowRoot.getElementById("gauge").getContext("2d");
-    new Chart(ctx, {
-      type: "doughnut",
-      data: { datasets: [{ data: [this._value, 100 - this._value], backgroundColor: ["#00aaff", "#eeeeee"], borderWidth: 0 }]},
-      options: { circumference: Math.PI, rotation: Math.PI, cutout: "80%", plugins: { legend: { display: false } } }
-    });
-  }
-}
+		set color(newColor) {
+			this._shadowRoot.getElementById("bps_color").value = newColor;
+		}
 
-customElements.define("custom-gauge", GaugeChart);
+		get color() {
+			return this._shadowRoot.getElementById("bps_color").value;
+		}
+	}
+
+	customElements.define("com-demo-box-bps", BoxBps);
+})();
